b711b629d1b91420fbc70e4d08e7b2f6347a85a8
Chogre <rlippy4@gmail.com>
2024-12-26 12:41:02 -0500

    On feature/spectrum_analyzer intial feature commit

---
+++ spectrum_anal.m
@@ -0,0 +1,12 @@
+#!/bin/octave
+% spectrum_anal.m
+% created on: 12/26/2024 by Chogre
+% Description: Spectrum analalysis for audio samples.
+% Changes:
+% 12/26/24: Intial Commit
+
+clear;clc;
+
+a=1
+fprintf("hello world\n")
+fprintf("hello Octave_ debugger")
